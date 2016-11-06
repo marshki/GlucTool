a50fffabedca2b02a2a50c6390fa1cf7c2b2b817
marshki <marshall@morethantomorrow.org>
2016-11-06 13:28:25 -0500

    Fix typo

--- gluc_tool_2.0.py
+++ gluc_tool_2.0.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3 
-
 """
     Program converts plasma glucose levels from:
     
-    * milligram per declieter to millimole per liter (mg/dl to mmol/l)
+    * milligram per decileter to millimole per liter (mg/dl to mmol/l)
     * millimole per liter to milligram per decileter (mmol/l to mg/dl). 
 
     Program accepts CLI parsing, but can also be run as a menu-driven program. 
