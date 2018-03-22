827196ddf8c8d34a0dcfef250bcbfcde35329a96
marshki <marshall@morethantomorrow.org>
2018-03-22 19:10:29 -0400

    fix EOL string literal

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   packages = ['GlucTool'], 
   version = '0.1',
   description = 'Python 3 utility for converting plasma glucose ("blood sugar") units.',
-  author = 'marshki,
+  author = 'marshki',
   url = 'https://github.com/marshki/GlucTool',
   keywords = ['blood sugar', 'glucose', 'conversion', 'utility'], 
   classifiers = [],
