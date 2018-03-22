b0e5937dd1f16b7d7c15fa17835f49218ddaf792
marshki <marshall@morethantomorrow.org>
2018-03-22 19:06:43 -0400

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+  name = 'GlucTool',
+  packages = ['GlucTool'], 
+  version = '0.1',
+  description = 'Python 3 utility for converting plasma glucose ("blood sugar") units.',
+  author = 'marshki,
+  url = 'https://github.com/marshki/GlucTool',
+  keywords = ['blood sugar', 'glucose', 'conversion', 'utility'], 
+  classifiers = [],
+)
