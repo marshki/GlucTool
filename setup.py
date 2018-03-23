9082d789137acb252cbbba1cf09baea13194b294
marshki <marshall@morethantomorrow.org>
2018-03-23 19:33:27 -0400

    add all

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from distutils.core import setup
+from setuptools import setup 
 
 setup(
   name = 'GlucTool',
-  version = '0.1',
   packages = ['GlucTool'], 
+  version = '0.1',
   description = 'Utility for converting plasma glucose ("blood sugar") units.',
-  author = 'Marshall J. Krinitz', 
-  author_email = 'marshall@morethantomorrow.org', 
+  author = 'Marshall J. Krinitz',
+  author_email = 'marshall[at]morethantomorrow.org',
   url = 'https://github.com/marshki/GlucTool',
-  downlaod_url = 'https://github.com/marshki/GlucTool/archive/0.1.tar.gz',
+  download_url = 'https://github.com/marshki/GlucTool/archive/0.1.tar.gz',
+  license = 'MIT',
   keywords = ['blood sugar', 'glucose', 'conversion', 'utility' 'mg' 'mmol'], 
-  classifiers = [],
 )
