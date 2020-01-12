7d213e8016f293b5e7f83009d61c1f3914db88ef
marshki <marshall@morethantomorrow.org>
2020-01-12 08:24:06 -0500

    fix pylint issues

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
+"""Setup file.
+"""
+
 from setuptools import setup
+
 setup(
-  name='GlucTool',
-  version='0.1.0',
-  description='Utility for converting plasma glucose ("blood sugar") units.',
-  py_modules=['GlucTool'],
-  author='Marshall J. Krinitz',
-  author_email='mjk235@nyu.edu',
-  url='https://github.com/marshki/GlucTool',
-  download_url='https://github.com/marshki/GlucTool/archive/0.1.tar.gz',
-  license='MIT',
-  keywords=['blood sugar' 'glucose' 'conversion' 'utility' 'mg' 'mmol'],
-  install_requires=['argparse'],
+    name='GlucTool',
+    version='0.1.1',
+    description='Utility for converting plasma glucose ("blood sugar") units.',
+    py_modules=['GlucTool'],
+    author='Marshall J. Krinitz',
+    author_email='mjk235@nyu.edu',
+    url='https://github.com/marshki/GlucTool',
+    download_url='https://github.com/marshki/GlucTool/archive/0.1.1.tar.gz',
+    license='MIT',
+    keywords=['blood sugar', 'glucose', 'conversion', 'utility', 'mg', 'mmol'],
+    install_requires=['argparse'],
 )
