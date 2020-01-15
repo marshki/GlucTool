63bf2bc01e8e14548fca0293a9d1927487fcee8d
marshki <marshall@morethantomorrow.org>
2020-01-15 11:17:35 -0500

    update

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -1,22 +1,36 @@
 #!/usr/bin/env python3
+# pylint: disable=W0613,W0622
 
 """Unit testing class.
 """
 
+import unittest
+from unittest.mock import patch
+
 def menu():
     """Menu prompt.
-    Args: User-defined input.
-    Returns: Choice if arg in: 1, 2, or 3.
-    Raises:
     """
 
     while True:
-        print('1. Convert plasma glucose level from mg/dl to mmol/l.')
-        print('2. Convert plasma glucose level from mmol/l to mg/dl.')
-        print('3. Quit.')
-        print()
         choice = input('Select an option (1, 2 or 3):  ')
         if choice in ('1', '2', '3'):
             return choice
 
-menu()
+class PlayAgainTest(unittest.TestCase):
+    """Testing class.
+    """
+
+    @patch('builtins.input', return_value='1')
+    def test_menu_01(self, input):
+        """Valid return values.
+        """
+        self.assertIn(menu(), ('1', '2', '3'))
+
+    @patch('builtins.input', return_value='3')
+    def test_menu_02(self, input):
+        """Valid return values.
+        """
+        self.assertIn(menu(), ('1', '2', '3'))
+
+if __name__ == '__main__':
+    unittest.main()
