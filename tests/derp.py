0385cbc8579d635b04dbcb2c67dfbcd15ef7d6ed
marshki <marshall@morethantomorrow.org>
2022-09-05 16:01:48 -0400

    add

---
+++ tests/derp.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env/bash
+
+import argparse
+import sys
+import unittest
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--name', required=True)
+    args = parser.parse_args()
+    print(f'Hello {args.name}')
+
+if __name__ == '__main__':
+    sys.exit(main())
+
+from main import main
+
+def test(capsys):
+    main(["--name", "Moish"])
+    captured = capsys.readouterr()
+    assert captured.out == "Hello Moish\n"
+
+
