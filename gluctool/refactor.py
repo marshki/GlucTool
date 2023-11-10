e615716a9f1a54be4e942b53f5729f30fcfea00f
marshki <marshall@morethantomorrow.org>
2023-11-10 10:25:05 -0500

    add arg parsing only

---
+++ gluctool/refactor.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+"""Argumet parser to onverts blood glucose levels between Intl. and US standards.
+"""
+
+import argparse
+
+MMOL_TO_MG = 18.0182
+
+def parse_cli_args():
+    """Define parser w/arguments.
+    """
+
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--mg-to-mmol", help="mg/dl to mmol/l", nargs='+', type=float)
+    group.add_argument("--mmol-to-mg", help="mmol/l to mg/dl", nargs='+', type=float)
+    args = parser.parse_args()
+    return args
+
+def convert_mmol_to_mg(x_value):
+    """Convert mmol/l to mg/dl equivalent.
+    """
+    return x_value*MMOL_TO_MG
+
+def convert_mg_to_mmol(x_value):
+    """Convert mg/dl to mmol/l equivalent.
+    """
+    return x_value/MMOL_TO_MG
+
+def conv_table(col1, col2_func, c1_hdr, c2_hdr):
+    """Formatted table with (2) columns and (2) headers.
+    """
+
+    print('+------------+------------+')
+    print('| {:^10} | {:^10} |'.format(c1_hdr, c2_hdr))
+    print('+------------+------------+')
+    for x_value in col1:
+        y_value = col2_func(x_value)
+        print('| {:10.4f} | {:10.4f} |'.format(x_value, y_value))
+    print('+------------+------------+')
+
+def user_float():
+    """User-defined floating point int.
+    Args: Value to convert.
+    Returns: Floating point int.
+    Raises: ValueError.
+    """
+
+    while True:
+        try:
+            return float(input('Enter plasma glucose level to convert:  '))
+        except ValueError:
+            print('Bad value, try again.')
+
+if __name__ == '__main__':
+    args = parse_cli_args()
+
+    if args.mg_to_mmol:
+        conv_table(args.mg_to_mmol, convert_mg_to_mmol, 'mg/dl', 'mmol/l')
+    if args.mmol_to_mg:
+        conv_table(args.mmol_to_mg, convert_mmol_to_mg, 'mmol/l', 'mg/dl')
