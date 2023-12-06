9a5597b10fa7704dd2c20414e4e14d4a251618cd
marshki <marshall@morethantomorrow.org>
2023-12-06 10:48:42 -0500

    add docstrings for unittests

--- tests/gluctool_test.py
+++ tests/gluctool_test.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+"""Unit testing class for gluctool.
+"""
+
 import unittest
 import textwrap
 from unittest.mock import patch
@@ -8,27 +11,40 @@ from gluctool.gluctool import parse_cli_args, convert_mmol_to_mg, convert_mg_to_
 
 class TestGluctool(unittest.TestCase):
 
+    """Unit tests.
+    """
+
     def test_parse_cli_args_mg_to_mmol(self):
+        """Test argument parser.
+        """
         with patch('sys.argv', ['script_name', '--mg-to-mmol', '100', '150']):
             args = parse_cli_args()
         self.assertEqual(args.mg_to_mmol, [100, 150])
         self.assertIsNone(args.mmol_to_mg)
 
     def test_parse_cli_args_mmol_to_mg(self):
+        """Test argument parser.
+        """
         with patch('sys.argv', ['script_name', '--mmol-to-mg', '5.5', '6.0']):
             args = parse_cli_args()
         self.assertEqual(args.mmol_to_mg, [5.5, 6.0])
         self.assertIsNone(args.mg_to_mmol)
 
     def test_convert_mmol_to_mg(self):
+        """Test conversion.
+        """
         result = convert_mmol_to_mg(5.0)
         self.assertAlmostEqual(result, 90.091)
 
     def test_convert_mg_to_mmol(self):
+        """Test conversion.
+        """
         result = convert_mg_to_mmol(90.091)
         self.assertAlmostEqual(result, 5.0)
 
     def test_conv_table(self):
+        """Test table output.
+        """
         col1 = [100, 150]
         col2_func = convert_mg_to_mmol
         c1_hdr = 'mg/dl'
